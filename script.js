0095068a37ade61ab9f71181da0a07f22f2c2bca
Egoisto69 <ruslanm@metropolia.fi>
2026-02-06 20:17:31 +0200

    npc-prototypes

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-const npc = {
+const npc01 = {
   name: "Morgan",
   role: "security guard",
   personality: "calm, evasive",
   memory: []
 };
 
+const npc02 = {
+  name: "Lola",
+  role: "hotel guest no.1",
+  personality: "A confident, slightly cocky regular",
+  memory: []
+};
 
-document.getElementById("send").onclick = () => {
+
+document.getElementById("sendToLola").onclick = () => {
   const text = document.getElementById("input").value;
 
   // NPC "запоминает" вопрос
-  npc.memory.push({
+  npc02.memory.push({
     from: "player",
     text: text
   });
 
   // NPC "отвечает"
-  const reply = `(${npc.name} remembers ${npc.memory.length} messages)`;
+  const reply = `(${npc02.name} remembers ${npc02.memory.length} messages)`;
 
-  npc.memory.push({
+  npc02.memory.push({
     from: "npc",
     text: reply
   });
 
-  document.getElementById("output").textContent = reply;
+  document.getElementById("output").textContent =
+  JSON.stringify(npc02.memory, null, 2);
+};
+
+document.getElementById("sendToMorgan").onclick = () => {
+  const text = document.getElementById("input").value;
+
+  // NPC "запоминает" вопрос
+  npc01.memory.push({
+    from: "player",
+    text: text
+  });
+
+  // NPC "отвечает"
+  const reply = `(${npc01.name} remembers ${npc01.memory.length} messages)`;
+
+  npc01.memory.push({
+    from: "npc",
+    text: reply
+  });
+
+  document.getElementById("output").textContent =
+  JSON.stringify(npc01.memory, null, 2);
 };
