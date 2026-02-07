1524918ac3399f44daef53fbd50178357e442ca0
Egoisto69 <ruslanm@metropolia.fi>
2026-02-07 16:09:28 +0200

    UI(1 part)

--- game.js
+++ game.js
@@ -30,11 +30,29 @@
 
   function preload() {
     // No assets – we draw shapes in create
+    this.load.image('bg', 'images/walls.png');
+    this.load.image('floor', 'images/floor.png');
+
+    this.load.image('hero_n', 'images/hero/north.png');
+    this.load.image('hero_ne', 'images/hero/north-east.png');
+    this.load.image('hero_e', 'images/hero/east.png');
+    this.load.image('hero_se', 'images/hero/south-east.png');
+    this.load.image('hero_s', 'images/hero/south.png');
+    this.load.image('hero_sw', 'images/hero/south-west.png');
+    this.load.image('hero_w', 'images/hero/west.png');
+    this.load.image('hero_nw', 'images/hero/north-west.png');
   }
 
   function create() {
+    //world
+    var zone = { x: 50, y: 40, width: 700, height: 425 };
+    this.physics.world.setBounds(zone.x, zone.y, zone.width, zone.height);
+    this.add.image(400, 250, 'bg').setOrigin(0.5, 0.5).setDepth(-2);
+    this.add.image(400, 250, 'floor').setOrigin(0.5, 0.5).setDepth(-1);
+
     // Player (Sheriff) – blue rectangle
-    player = this.add.rectangle(400, 250, 32, 48, 0x3a7ca5);
+    player = this.add.sprite(56, 56, 'hero_s') // по умолчанию смотрит на юг
+          .setScale(2);
     this.physics.add.existing(player, false);
     player.body.setCollideWorldBounds(true);
 
@@ -83,10 +101,45 @@
     body.setVelocity(0);
 
     var keys = this.moveKeys;
-    if (keys.up.isDown) body.setVelocityY(-speed);
-    if (keys.down.isDown) body.setVelocityY(speed);
-    if (keys.left.isDown) body.setVelocityX(-speed);
-    if (keys.right.isDown) body.setVelocityX(speed);
+    let dx = 0;
+    let dy = 0;
+    let moving = false;
+
+    // Определяем направление по нажатым клавишам
+    if (keys.up.isDown) {
+        dy = -1;
+        moving = true;
+    }
+    if (keys.down.isDown) {
+        dy = 1;
+        moving = true;
+    }
+    if (keys.left.isDown) {
+        dx = -1;
+        moving = true;
+    }
+    if (keys.right.isDown) {
+        dx = 1;
+        moving = true;
+    }
+
+    // Применяем скорость
+    body.setVelocityX(dx * speed);
+    body.setVelocityY(dy * speed);
+
+    // Смена спрайта в зависимости от направления
+    if (!moving) {
+        player.setTexture('hero_s'); // стоит → смотрит на юг
+    } else {
+        if (dx === 0 && dy === -1) player.setTexture('hero_n');
+        else if (dx === 1 && dy === -1) player.setTexture('hero_ne');
+        else if (dx === 1 && dy === 0) player.setTexture('hero_e');
+        else if (dx === 1 && dy === 1) player.setTexture('hero_se');
+        else if (dx === 0 && dy === 1) player.setTexture('hero_s');
+        else if (dx === -1 && dy === 1) player.setTexture('hero_sw');
+        else if (dx === -1 && dy === 0) player.setTexture('hero_w');
+        else if (dx === -1 && dy === -1) player.setTexture('hero_nw');
+    }
   }
 
   new Phaser.Game(config);
